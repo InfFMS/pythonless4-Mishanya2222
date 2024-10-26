c842d7745131c9f5b114c8f9390deb34555bd69a
Mishanya2222 <146630550+Mishanya2222@users.noreply.github.com>
2024-10-26 17:29:15 +0300

    Update task7.py

--- task7.py
+++ task7.py
@@ -2,10 +2,11 @@
 # которая принимает в качестве аргументов три натуральных числа,
 # и возвращает значение True, если существует невырожденный треугольник
 # со сторонами side1, side2, side3, или False в противном случае.
+a = None
 def funk(x, y, z):
     if x+y<z and x + z < y and z + y < x:
-        print(True)
+        a = True
     else:
-        print(False)
-    return "досвидания"
+        a = Fals
+    return a
 print(funk(int(input()), int(input()), int(input())))
